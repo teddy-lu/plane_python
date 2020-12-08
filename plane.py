43adce3b3b04888e9b0a000beb82ba40e6636962
Teddy Luu <teddy@ifreegroup.com>
2020-12-08 12:19:00 +0800

    添加检测事件，比如按键操作

--- plane.py
+++ plane.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 import pygame
+from pygame.locals import *
 
 '''
     1.搭建界面，主要完成窗口和背景图显示
+    2.用来检测事件，比如按键操作
 '''
 
 
@@ -17,6 +19,25 @@ def main():
     while True:
         # 设定显示的背景图
         screen.blit(background, (0, 0))
+
+        # 获取事件，比如按键等
+        for event in pygame.event.get():
+            # 判断是否点击了退出
+            if event.type == QUIT:
+                print("exit")
+                exit()
+            # 判断是否是按下了键
+            elif event.type == KEYDOWN:
+                # 检测按键是否是a或者left
+                if event.key == K_a or event.key == K_LEFT:
+                    print("left")
+                # 检测按键是否是d或者right
+                elif event.key == K_d or event.key == K_RIGHT:
+                    print("right")
+                # 检测按键是否是空格键
+                elif event.key == K_SPACE:
+                    print("space")
+
         # 更新显示的内容
         pygame.display.update()
 
