d801c7cd0af98346e6486efe4b9745df2f3b5cb5
Teddy Luu <teddy@ifreegroup.com>
2020-12-08 14:23:38 +0800

    使用面向对象的方式显示飞机，以及控制左右移动

--- plane.py
+++ plane.py
@@ -6,7 +6,55 @@ from pygame.locals import *
     1.搭建界面，主要完成窗口和背景图显示
     2.用来检测事件，比如按键操作
     3.使用面相过程的方式显示一个飞机，并控制其左右移动
+    4.使用面向对象的方式显示飞机，以及控制左右移动
+        1.实现飞机在你想要的位置显示
+        2.实现按键控制飞机移动
 '''
+
+
+class HeroPlane(object):
+    def __init__(self, screen):
+        # 设置飞机位置
+        self.x = 230
+        self.y = 700
+
+        # 设置窗口
+        self.screen = screen
+
+        self.imgName = "./plane/hero1.png"
+
+        self.image = pygame.image.load(self.imgName).convert()
+
+    def display(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
+    def moveLeft(self):
+        self.x -= 10
+
+    def moveRight(self):
+        self.x += 10
+
+
+def key_control(heroPlane):
+    # 获取事件，比如按键等
+    for event in pygame.event.get():
+        # 判断是否点击了退出
+        if event.type == QUIT:
+            print("exit")
+            exit()
+        # 判断是否是按下了键
+        elif event.type == KEYDOWN:
+            # 检测按键是否是a或者left
+            if event.key == K_a or event.key == K_LEFT:
+                print("left")
+                heroPlane.moveLeft()
+            # 检测按键是否是d或者right
+            elif event.key == K_d or event.key == K_RIGHT:
+                print("right")
+                heroPlane.moveRight()
+            # 检测按键是否是空格键
+            elif event.key == K_SPACE:
+                print("space")
 
 
 def main():
@@ -16,38 +64,16 @@ def main():
     # 2 创建一个盒窗口大小的图片，用了填充背景
     background = pygame.image.load("./plane/background.png").convert()
 
-    # 测试，用来创建一个玩家飞机的图片
-    hero = pygame.image.load("./plane/hero1.png").convert()
-    # 飞机的x,y坐标
-    x = 0
-    y = 0
+    hero_plane = HeroPlane(screen)
 
     # 3 把背景图片放到窗口中显示
     while True:
         # 设定显示的背景图
         screen.blit(background, (0, 0))
-        # 设定飞机的图片
-        screen.blit(hero, (x, y))
 
-        # 获取事件，比如按键等
-        for event in pygame.event.get():
-            # 判断是否点击了退出
-            if event.type == QUIT:
-                print("exit")
-                exit()
-            # 判断是否是按下了键
-            elif event.type == KEYDOWN:
-                # 检测按键是否是a或者left
-                if event.key == K_a or event.key == K_LEFT:
-                    print("left")
-                    x -= 5
-                # 检测按键是否是d或者right
-                elif event.key == K_d or event.key == K_RIGHT:
-                    print("right")
-                    x += 5
-                # 检测按键是否是空格键
-                elif event.key == K_SPACE:
-                    print("space")
+        hero_plane.display()
+
+        key_control(hero_plane)
 
         # 更新显示的内容
         pygame.display.update()
